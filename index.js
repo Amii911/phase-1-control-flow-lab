61ec786be1875dd9e7b3ce5debdc5d763958c04f
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-01-15 18:40:51 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
-function scuberGreetingForFeet(feet){
-  if(feet <= 400) {
-    return 'This one is on me!'
-  }else if(feet > 2000 && feet < 2500) {
-    return 'I will gladly take your thirty bucks.'
-  } else if(feet >= 2500) {
-    return 'No can do.'
-  } 
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (2000 < feet && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
+
 
 function ternaryCheckCity(city){
   return city === "NYC" ? "Ok, sounds good." : "No go."
